3982b58e5e71510d25424ad1c9ad3137bdfe30f7
nramiscal <nramiscal@gmail.com>
2019-03-28 01:08:56 -0400

    refactored

--- rFib_with_memoization.py
+++ rFib_with_memoization.py
@@ -5,11 +5,9 @@ def fibonacci(n):
     if n in fibonacci_cache:
         return fibonacci_cache[n]
 
-    if n == 0:
-        value = 0
-    elif n == 1:
-        value = 1
-    elif n >= 2:
+    if n < 2:
+        value = n
+    else:
         value = fibonacci(n-1) + fibonacci(n-2)
 
     fibonacci_cache[n] = value
